24dc55d7a229808c38c01ea67d73693cfd3ae16f
felix5959 <121395181@umail.ucc.ie>
2023-12-06 22:49:48 +0000

    Changed the colours variable to an ArrayList of Colors, added a new int variable called capacity to track a palette's capacity and created a test for the add() method before coding it

--- src/main/java/assignment3tests/ColourTable.java
+++ src/main/java/assignment3tests/ColourTable.java
@@ -1,14 +1,25 @@
 package assignment3tests;
 
+import java.awt.Color;
+import java.util.ArrayList;
+
 public class ColourTable {
-    public String[] colours;
+    public int capacity;
+    public ArrayList<Color> colours;
 
     public ColourTable(int paletteCapacity) {
         if (paletteCapacity != 2 & paletteCapacity != 4 & paletteCapacity != 8 & paletteCapacity != 16 & paletteCapacity != 32 & paletteCapacity != 64 & paletteCapacity != 128 & paletteCapacity != 256 & paletteCapacity != 512 & paletteCapacity != 1024) {
             throw new ArithmeticException("Number of colours in the palette must be 2, 4, 8, 16, 32, 64, 128, 256, 512 or 1024");
         }
         else {
-            colours = new String[paletteCapacity];
+            capacity = paletteCapacity;
+            colours = new ArrayList<>();
         }
+    }
+
+    public void add(String colourString) {
+        String[] rgb = colourString.split(",");
+        Color colour = new Color(Integer.parseInt((rgb[0])), Integer.parseInt((rgb[1])), Integer.parseInt((rgb[2])));
+        colours.add(colour);
     }
 }
--- src/test/java/assignment3tests/SetTableTests.java
+++ src/test/java/assignment3tests/SetTableTests.java
@@ -3,6 +3,7 @@ package assignment3tests;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.awt.Color;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SetTableTests {
@@ -10,7 +11,18 @@ public class SetTableTests {
     @ValueSource(ints = {2,4,8,16,32,64,128,256,512,1024})
     public void tableCreation(int value) {
         ColourTable table = new ColourTable(value);
-        String[] expected = new String[value];
-        assertEquals(expected.length, table.colours.length);
+        Color[] expected = new Color[value];
+        assertEquals(expected.length, table.capacity);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"255,0,0", "0,255,0", "0,0,255"})
+    public void addColour(String value) {
+        ColourTable table = new ColourTable(4);
+        table.add(value);
+        String[] values = value.split(",");
+        Color expected = new Color(Integer.parseInt((values[0])), Integer.parseInt((values[1])), Integer.parseInt((values[2])));
+        assertEquals(expected, table.colours.get(0));
+    }
+
 }
